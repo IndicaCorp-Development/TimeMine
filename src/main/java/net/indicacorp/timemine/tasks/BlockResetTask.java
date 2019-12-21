8908dddbb2bd479981cd68485791f1ebd303da1e
Dalton <dpelkey98@outlook.com>
2019-12-21 16:14:15 -0600

    Revert "Schedule block removal in TimeMineBlock.setBlockType"
    This reverts commit e596276b1c047a91a28395e107b43a8aca5c8b10.

--- src/main/java/net/indicacorp/timemine/tasks/BlockResetTask.java
+++ src/main/java/net/indicacorp/timemine/tasks/BlockResetTask.java
@@ -2,40 +2,29 @@ package net.indicacorp.timemine.tasks;
 
 import net.indicacorp.timemine.models.TimeMineBlock;
 import net.indicacorp.timemine.util.BlockCache;
-
-import java.util.*;
-//import java.util.concurrent.Executors;
-//import java.util.concurrent.ScheduledExecutorService;
-//import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class BlockResetTask {
-//    private ScheduledExecutorService executorService;
-    private Timer timer;
+    private ScheduledExecutorService executorService;
     private int period;
     private boolean paused;
 
     public BlockResetTask(int interval) {
-//        executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService = Executors.newSingleThreadScheduledExecutor();
         period = interval;
         paused = false;
-        timer = new Timer();
     }
 
     public void stop() { paused = true; }
     public void start() { paused = false; }
 
-//    public void initTask() {
-//        executorService.scheduleAtFixedRate(() -> {
-//
-//        }, 0, period, TimeUnit.SECONDS);
-//    }
     public void initTask() {
-        timer.scheduleAtFixedRate(new Task(), 0, period*1000);
-    }
-
-    private class Task extends TimerTask {
-        @Override
-        public void run() {
+        executorService.scheduleAtFixedRate(() -> {
             if (paused) return;
             paused = true;
 
@@ -45,7 +34,6 @@ public class BlockResetTask {
 
             for (Map.Entry<String, TimeMineBlock> entry : cache.entrySet()) {
                 TimeMineBlock b = entry.getValue();
-                if (!b.isMined()) continue;
                 int resetInterval = b.getResetInterval();
                 Date minedAt = b.getMinedAt();
                 Date expiredAt = new Date(minedAt.getTime() + (resetInterval * 1000));
@@ -61,6 +49,6 @@ public class BlockResetTask {
             }
 
             paused = false;
-        }
+        }, 0, period, TimeUnit.SECONDS);
     }
 }
