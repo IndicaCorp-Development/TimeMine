da604042cc0e66a67c316727360ea7bf9ae5d4e1
Dalton <dpelkey98@outlook.com>
2019-12-23 17:24:32 -0600

    Pull tool configuration and run check on block break

--- src/main/java/net/indicacorp/timemine/models/Tool.java
+++ src/main/java/net/indicacorp/timemine/models/Tool.java
@@ -2,36 +2,37 @@ package net.indicacorp.timemine.models;
 
 import net.indicacorp.timemine.TimeMine;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Tool {
-    private TimeMine plugin;
     private Material tool;
-    private HashMap<Material, ArrayList<Material>> mineables;
-    private FileConfiguration config;
+    private static FileConfiguration config;
+    private static HashMap<String, List<?>> mineables = null;
 
-    public Tool(Material material) {
-        plugin = TimeMine.getInstance();
-        tool = material;
-        config = plugin.getConfig();
+    public Tool(Material tool) {
+        this.tool = tool;
+        config = TimeMine.getInstance().getConfig();
         loadFromConfig();
     }
 
-    private void loadFromConfig() {
-        List<String> mineables = config.getStringList("timemine.mineables");
-//        for (String e : mineables) {
-//
-//        }
-//        System.out.println(mineables);
+    private static void loadFromConfig() {
+        ConfigurationSection section = config.getConfigurationSection("mineables");
+        if (section == null) return;
+        mineables = new HashMap<>();
+        for (String e : section.getKeys(false)) {
+            mineables.put(e, config.getList("mineables." + e));
+        }
     }
 
-    public boolean canMine(Material material) {
-        HashMap<String, Material> validTools = new HashMap<String, Material>();
-        return true;
+    public boolean canMine(Material block) {
+        String toolEnum = tool.toString();
+        if (!mineables.containsKey(toolEnum)) return false;
+        List<?> mineableEnums = mineables.get(toolEnum);
+        return mineableEnums.contains(block.toString());
     }
 }
 
